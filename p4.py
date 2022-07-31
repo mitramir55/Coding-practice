290b85be318da015811968fa751883929c06d671
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-07-31 14:35:57 -0600

    second p

--- p4.py
+++ p4.py
@@ -6,3 +6,27 @@ square_digits(9119)
 
 for i in map(int, str(520)):
     print(i)
+
+def create_dict(word):
+    word_letters = list(word)
+    counts = dict()
+
+    for letter in word_letters:
+        counts[letter] = counts.get(letter, 0) + 1
+        
+    return counts
+
+def anagrams(word, words):
+
+    accepted_words = []
+    reference_dict = create_dict(word)
+
+    for candidate in words:
+        counts_dict = create_dict(candidate)
+        if reference_dict == counts_dict:accepted_words.append(candidate) 
+
+    return accepted_words
+
+anagrams(word='mitikoman', words=['miti', 'moto', 'mtiikoman'])
+
+sorted('mitikoman')
