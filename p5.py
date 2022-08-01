2953bf1195564ec4d9c2a5c23e002d0592a40801
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-08-01 14:33:30 -0600

    2nd of the day

--- p5.py
+++ p5.py
@@ -68,3 +68,47 @@ lower = str.maketrans(ascii_lowercase, ascii_lowercase[13:] + ascii_lowercase[:1
 ascii_lowercase[13:] + ascii_lowercase[:13]
 
 ascii_lowercase[13:] + ascii_lowercase[:13]
+import string
+string.ascii_lowercase
+
+import string
+
+
+
+all_letters = string.ascii_lowercase
+alphabet_dict = {}
+for i in range(1, len(all_letters)+1): 
+    alphabet_dict[all_letters[i-1]] = i
+
+
+
+
+        
+import string
+
+all_letters = string.ascii_lowercase
+alphabet_dict = {}
+for i in range(1, len(all_letters)+1): 
+    alphabet_dict[all_letters[i-1]] = i
+
+        
+        
+def alphabet_position(text: str) -> str:
+    numbers_list = []
+    
+    for letter in text:
+        
+        if letter.lower() in alphabet_dict.keys(): 
+            
+            number_str = str(alphabet_dict[letter.lower()])
+            numbers_list.append(number_str)
+        
+    return " ".join(numbers_list)
+
+ord('b')
+
+def alphabet_position(text:str) ->str:
+    return " ".join(str(ord(letter)-96) for letter in text if letter.isalpha())
+alphabet_position(
+    'miti'
+)
