6ab6d4c4fb458965859b8bfbf55eeaf3e8e1e586
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-08-16 00:20:18 -0600

    heads and legs

--- DS and AL/p3.py
+++ DS and AL/p3.py
@@ -19,7 +19,29 @@ def findPeakUtil(arr, lowerBound, upperBound, n):
         return findPeakUtil(arr, lowerBound, (mid - 1), n)
 
 
+# doesn't work for this. we have to sort it first
 arr = [10, 20, 23, 2, 1, 90, 67]
 n = len(arr)
 print("The peak point is", arr[findPeakUtil(arr, 0, n - 1, n)])
 
+
+# bisection
+arr = list(range(0, 100))
+
+def find_g(g, lower_b, upper_b, arr=arr):
+    """
+    g: guess
+    """
+    mid = lower_b + (upper_b-lower_b)//2
+    print(f'mid is {mid}')
+    if arr[mid] == g: 
+        print('that is it!')
+        return mid
+    elif arr[mid]<g: 
+        print('going down!')
+        find_g(g, lower_b=mid, upper_b=upper_b)
+    else: 
+        print('going up!')
+        find_g(g, lower_b=lower_b, upper_b=mid)
+
+find_g(56, lower_b=0, upper_b=99)
--- p13.py
+++ p13.py
@@ -11,3 +11,29 @@ def funct_(prod):
     return [a, b, False]
 
 funct_(6)
+
+
+
+def binary_array_to_number(arr):
+    return int("".join(map(str, arr)), 2)
+
+arr = [0, 1, 0, 1]
+
+print("".join(map(str, arr)))
+
+h = 0
+a = 0
+def cal(heads, legs):
+    h = 0
+    a = 0
+    all_a = legs//4
+    if all_a == heads: return h, all_a
+    else:
+        while heads!= h+a:
+            a = all_a - 4
+            h += 1
+        return h, a
+cal(heads=22, legs=72)
+
+
+    return h, a
