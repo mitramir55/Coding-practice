701f15150dafdcae6516828d1142fdece789ee49
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-08-20 22:02:29 -0600

    graphs

--- DS and AL/6.0002/main_session3.py
+++ DS and AL/6.0002/main_session3.py
@@ -101,12 +101,16 @@ def DFS(graph, start, end, path, shortest, toPrint = False):
           path and shortest are lists of nodes
        Returns a shortest path from start to end in graph"""
     path = path + [start]
+
     if toPrint:
         print('Current DFS path:', printPath(path))
+        
     if start == end:
         return path
+
     for node in graph.childrenOf(start):
-        if node not in path: #avoid cycles
+        if node not in path: 
+            #avoid cycles
             if shortest == None or len(path) < len(shortest):
                 newPath = DFS(graph, node, end, path, shortest,
                               toPrint)
--- DS and AL/6.0002/session3_graphs.py
+++ DS and AL/6.0002/session3_graphs.py
@@ -1,3 +1,6 @@
+from csv import Dialect
+
+
 class Node:
     def __init__(self, name) -> None:
         self.name = name
@@ -45,8 +48,7 @@ class Digraph:
         if not dest in self.edges:
             raise ValueError('Destination node not in graph.')
 
-        else:
-            self.edges[src].append(dest)
+        self.edges[src].append(dest)
 
     def get_children(self, node):
         return self.edges[node]
@@ -72,9 +74,11 @@ class Digraph:
 
 class Graph(Digraph):
     def add_edge(self, edge):
-        Digraph.add_edge(edge)
-        reversed_edge = Edge(scr=edge.scr, dest=edge.dest)
-        Digraph.add_edge(reversed_edge)
+
+        Digraph.add_edge(self, edge)
+
+        reversed_edge = Edge(scr=edge.get_source(), dest=edge.get_dest())
+        Digraph.add_edge(self, reversed_edge)
 
 def build_city_graph(graph_type):
 
@@ -87,14 +91,115 @@ def build_city_graph(graph_type):
     for name in cities:
         g.add_node(Node(name))
 
-    g.add_edge(g.get_node('Boston'), g.get_node('Providence'))
-    g.add_edge(g.get_node('Boston'), g.get_node('New York'))
-    g.addEdge(Edge(g.getNode('Providence'), g.getNode('Boston')))
-    g.addEdge(Edge(g.getNode('Providence'), g.getNode('New York')))
-    g.addEdge(Edge(g.getNode('New York'), g.getNode('Chicago')))
-    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Denver')))
-    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Phoenix')))
-    g.addEdge(Edge(g.getNode('Denver'), g.getNode('Phoenix')))
-    g.addEdge(Edge(g.getNode('Denver'), g.getNode('New York')))
-    g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Boston')))
+    g.add_edge(Edge(g.get_node('Boston'), g.get_node('Providence')))
+    g.add_edge(Edge(g.get_node('Boston'), g.get_node('New York')))
+    g.add_edge(Edge(g.get_node('Providence'), g.get_node('Boston')))
+    g.add_edge(Edge(g.get_node('Providence'), g.get_node('New York')))
+    g.add_edge(Edge(g.get_node('New York'), g.get_node('Chicago')))
+    g.add_edge(Edge(g.get_node('Chicago'), g.get_node('Denver')))
+    g.add_edge(Edge(g.get_node('Chicago'), g.get_node('Phoenix')))
+    g.add_edge(Edge(g.get_node('Denver'), g.get_node('Phoenix')))
+    g.add_edge(Edge(g.get_node('Denver'), g.get_node('New York')))
+    g.add_edge(Edge(g.get_node('Los Angeles'), g.get_node('Boston')))
     return g
+
+
+def print_path(path):
+
+    result = ' '
+    for i in range(len(path)):
+        result += str(path[i])
+        if i != len(path)-1:
+            result += ' -> '
+    return result
+
+
+### ==================== SHORTEST PATH? ======================
+
+# depth first search
+def DFS(graph, start, end, path, shortest):
+
+    path = path + [start]
+    print('current path = ', print_path(path))
+
+    if end == start: return path
+    
+    for node in graph.get_children(start):
+        # avoid cycles
+        if node not in path:
+            if shortest == None or len(path) < len(shortest):
+                new_path = DFS(graph, node, end, path, shortest=None)
+                if new_path != None: shortest = new_path
+
+        else: print('Already visited!')
+
+    return shortest
+
+def get_shortest_path(graph, start, end):
+    return DFS(graph, start, end, path=[], shortest=None)
+
+def test_shortest_path(source, dest):
+
+    graph = build_city_graph(Digraph)
+    shortest_path = get_shortest_path(
+        graph,
+        start=graph.get_node(source),
+        end=graph.get_node(dest)
+        )
+
+    if shortest_path:
+        print(f'shortest path from {source} to {dest} is ')
+        print(print_path(shortest_path))
+
+    else: print(f'No path from {source} to {dest}!')
+
+
+test_shortest_path('Boston', 'Phoenix')
+
+
+
+# Breadth first search
+
+def BFS(graph, start, end, print_queue=True):
+
+    init_path = [start]
+    path_queue = [init_path]
+
+    while path_queue != []:
+
+        # print current queue
+        if print_queue:
+            print('length of queue = ', len(path_queue))
+
+            for path in path_queue: 
+                print(print_path(path))
+
+        # get the last one
+        temp_path = path_queue.pop(0)
+        end_node = temp_path[-1]
+        if end_node == end:
+            return temp_path
+
+        for next_node in graph.get_children(end_node):
+            print(type(next_node))
+            if next_node not in temp_path:
+                new_path = temp_path + [next_node]
+                path_queue.append(new_path)
+            else: print('already visited!')
+
+    return None
+
+def get_shortest_path(graph, start, end):
+    return BFS(graph, start, end, print_queue=True)
+
+
+
+test_shortest_path(source='Boston', dest='Phoenix')
+
+# listt = [['boston']]
+# f = listt.pop(0)
+# f
+# listt
+
+
+
