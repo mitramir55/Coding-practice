47998eebbfcf57ea5cbc5735372857a639e68aed
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-08-28 15:11:41 -0600

    first katas

--- DS and AL/youtube freecodecamp/p11_QuickSort.py
+++ DS and AL/youtube freecodecamp/p11_QuickSort.py
@@ -17,3 +17,14 @@ def quick_sort(arr):
 arr = [5, 0, 60, 2, 5, 4, 3, 5]
 
 quick_sort(arr)
+
+
+type(bin(-5))
+
+'{:b}'.format(-5)
+val1 = 5
+val2 = 2
+operation = '+'
+round(eval("{0}{1}{2}{1}".format(round(val1), round(val2), operation)))
+
+"{0}{1}{2}{1}".format(round(val1), round(val2), operation)
