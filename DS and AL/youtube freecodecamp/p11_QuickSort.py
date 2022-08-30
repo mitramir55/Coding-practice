477aa6ce13b777500a48a3fa750ea53a66ff96e9
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-08-29 18:59:38 -0600

    first attempts

--- DS and AL/youtube freecodecamp/p11_QuickSort.py
+++ DS and AL/youtube freecodecamp/p11_QuickSort.py
@@ -28,3 +28,92 @@ operation = '+'
 round(eval("{0}{1}{2}{1}".format(round(val1), round(val2), operation)))
 
 "{0}{1}{2}{1}".format(round(val1), round(val2), operation)
+
+
+cc = "1"
+'#' * (len(cc) - 4) + cc[-4:]
+
+11%6
+
+
+import math
+
+prime_bigger_forty = [ i**2 + i + 41 for i in range(0, 40)]
+prime_smaller_forty = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+
+def is_prime(num):
+    
+    # eliminate less than 1
+    if num <= 1: 
+        return False
+    
+    num_sqrt = int(math.sqrt(num))
+
+    if num in [2, 3]: return True
+
+    # check primes less than 40
+    for i in prime_smaller_forty: 
+
+        if i <= num_sqrt:
+            print('i = ', i)
+            if num == i:
+                return True
+            
+            # check if their devisable by any prime number
+            elif num % i == 0: 
+                return False
+        else: break
+
+    if num in prime_bigger_forty: return True
+
+    # before checking the large numbers,
+    # see if the square root is less than 40
+    # if it is, then we've checked it before to make sure the number is not 
+    # devisable by prime numbers < its sqrt 
+    if num > 41 and num_sqrt < 41:
+        return True
+
+
+is_prime(num=73)
+
+
+
+
+
+
+
+import math
+
+prime_bigger_forty = [ i**2 + i + 41 for i in range(0, 40)]
+prime_smaller_forty = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
+
+def is_prime(num):
+    
+    # eliminate less than 1
+    if num <= 1: 
+        return False
+    
+    num_sqrt = int(math.sqrt(num))
+
+    if num in prime_smaller_forty + prime_bigger_forty: return True
+
+    # check primes less than 40
+    for i in prime_smaller_forty + prime_bigger_forty: 
+
+        if i <= num_sqrt:
+            print('i = ', i)
+            
+            # check if their devisable by any prime number
+            if num % i == 0: 
+                return False
+        else: break
+    return True
+
+    # before checking the large numbers,
+    # see if the square root is less than 40
+    # if it is, then we've checked it before to make sure the number is not 
+    # devisable by prime numbers < its sqrt 
+    if num > 41 and num_sqrt < 41:
+        return True
+
+is_prime(73)
