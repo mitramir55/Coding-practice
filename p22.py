f2d2aa2b8c2cd62a91c0828f452ccbb463453246
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-09-03 17:40:54 -0600

    first

---
+++ p22.py
@@ -0,0 +1,30 @@
+p = 'UFFDDFDUDFUFUUFFDDFDUDFUFU'
+import regex  as re
+
+def counting_valleys(p): 
+
+    in_valley = False
+    n_valley = 0
+    base = 0
+
+    #dir_dict = {}
+    for d in p: 
+
+        # add if going up
+        # decrease if going down
+        if d == 'U': base += 1
+        elif d == 'D': base -= 1
+
+        # if we've gone down
+        if not in_valley and base < 0:
+            in_valley = True
+            n_valley += 1
+
+        # if we reached level 0
+        if in_valley and base == 0:
+            in_valley = False
+
+    return n_valley
+
+counting_valleys(p)
+
