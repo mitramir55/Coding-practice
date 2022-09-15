c357daedab294f96b26e6ce58edaca11328d794a
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-09-14 21:23:04 -0600

    for finding the middle one

---
+++ p26.py
@@ -0,0 +1,22 @@
+
+
+
+def helper(start, end, nums, target):
+    mid_idx = (end - start) // 2 + start
+
+    if nums[mid_idx] == target:
+        return mid_idx
+
+    if end == start + 1 and nums[mid_idx] != target: 
+        print('target = ', target)
+        print('nums = ', nums)
+        if target > nums[mid_idx]: return mid_idx + 1
+        else: return mid_idx
+
+    elif nums[mid_idx] > target: return helper(start, end = mid_idx, nums=nums, target=target)
+    else: return  helper(start = mid_idx, end = end, nums=nums, target=target)
+
+def searchInsert(nums, target: int) -> int:
+    return helper(start=0, end=len(nums), nums=nums, target=target)
+    
+searchInsert(nums=[1,2,3,4,5], target=5)
