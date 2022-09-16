1dd277384e39fcdb3e9fb0cbf11433ccbf08e88d
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-09-15 23:48:55 -0600

    java

--- j1.java
+++ j1.java
@@ -1,8 +1,23 @@
-private vid inOrderTraversal(TreeNode node) {
+private void inOrderTraversal(TreeNode node) {
     if (node==null) {
         return null;
     }
     inOrderTraversal(node.left);
     System.out.printf("%s", node.val);
     inOrderTraversal(node.right);
+}
+
+
+class Miti{
+    public  void main(String s) {
+        System.out.println("this is crazy!");
+    }
+}
+
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
---
+++ j2.java
@@ -0,0 +1,6 @@
+ class j2 {
+
+    public static void main(String[] ars) {
+        System.out.println("hi there!");
+    }
+}
