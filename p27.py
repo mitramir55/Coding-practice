fa05f42361bb049c3ff5049c8fcbb6ba0506bd79
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-09-16 23:08:23 -0600

    new leets

--- p27.py
+++ p27.py
@@ -19,3 +19,16 @@ class ListNode:
         self.left = left
         self.right = right
 
+def are_sym(root1, root2):
+
+	if root1.val == root2.val == None: return True
+	if not root1.val or not root2.val: return False
+	
+	if root1.val == root2.val:
+		return are_sym(root1.left, root2.right) and are_sym(root1.right, root2.left)
+
+def is_sym(root):
+	
+	if not root: return True
+	
+	return are_sym(root.left, root.right)
