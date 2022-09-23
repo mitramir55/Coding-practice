b6dc86c25b085c5e55292c0a012ba7b962f99a39
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-09-23 15:49:40 -0600

    created leet medium

--- practice/p29.py
+++ practice/p29.py
@@ -46,3 +46,72 @@ def get_diameter(root):
     if root:
         return get_diameter(root.left) + get_diameter(root.right) + 1
 
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        curr1 = l1
+        curr2 = l2
+        str1 = ''
+        str2 = ''
+        
+        while curr1:
+            str1 = str(curr1.val) + str1
+            curr1 = curr1.next
+            
+        while curr2:
+            str2 = str(curr2.val) + str2
+            curr2 = curr2.next
+        sum_int = int(str1) + int(str2)
+        str_sum = str(sum_int)
+        head = new_l = ListNode(val=int(str_sum[-1]))
+        
+        for i in str_sum[:-1][::-1]:
+            new_l.next = ListNode(val=int(i))
+            new_l = new_l.next
+            
+        return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, root1: Optional[ListNode], root2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        head = new_l = ListNode(val=0)
+        carry = 0
+        while root1 or root2 or carry:
+            val1, val2 = 0, 0
+            if root1:
+                val1 = root1.val
+                root1 = root1.next
+
+            if root2:
+                val2 = root2.val
+                root2 = root2.next
+
+            carry, val = divmod(carry+val1+val2, 10)
+            new_l.next = ListNode(val=val)
+            new_l = new_l.next
+            
+        return head.next
+
+# aabc
+def lengthOfLongestSubstring(s: str):
+    ret_str = ''
+    res = 0
+    for l in s:
+        res ^= ord(l)
+        print(res)
+        if res==0: break
+        ret_str += s
+    return ret_str
+
