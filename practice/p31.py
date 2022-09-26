b859218dacc1cf4d2123976d79982de1fa077e0b
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-09-26 07:28:25 -0600

    leet sudoku review

---
+++ practice/p31.py
@@ -0,0 +1,84 @@
+import numpy as np
+from itertools import chain
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        board = np.array(board)
+        
+        # checking horizontal arrays
+        for arr in board:
+            seen_set = set()
+            for element in arr:
+                if element == '.': continue
+                else: 
+                    if element in seen_set: 
+                        print('row false')
+                        return False
+                    seen_set.add(element)
+            
+        # checking for vertical columns
+        for arr in board.T:
+            seen_set = set()
+            
+            for element in arr:
+                if element == '.': continue
+                else: 
+                    if element in seen_set: 
+                        print('col false')
+                        return False
+                    seen_set.add(element)
+            
+        # checking the 3 x 3 boxes
+        for i in range(3):
+            sec1 = board[i*3: (i+1)*3]
+            
+            for j in range(3):
+                seen_set = set()
+                sec2 = sec1.T[j*3: (j+1)*3]
+                print('sec2: \n', sec2)
+                
+                for element in chain.from_iterable(sec2):
+                    if element == '.':
+                        continue
+                    else:
+                        if element in seen_set: 
+                            return False
+                        seen_set.add(element)
+                        
+                        
+        return True
+
+
+
+
+
+import numpy as np
+from itertools import chain
+
+
+def is_valid(arr):
+    arr = [k for k in arr if k != '.']
+    return len(arr) == len(set(arr))
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        # horizontal check
+        for arr in board:
+            if not is_valid(arr): return False
+
+        # vertical check
+        for arr in zip(*board):
+            if not is_valid(arr): return False
+
+        # for 3 x 3 matrixes
+        for i in range(3):
+            for j in range(3):
+                arr = [board[x][y] for x in range(i*3, (i+1)*3) 
+                for y in range(j*3, (j+1)*3)]
+
+                if not is_valid(arr): return False
+                
+        return True
+
