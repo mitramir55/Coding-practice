55f7ee39f46bf287032077cada889220aef41ac2
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-10-23 17:31:05 -0600

    problem with stacks

---
+++ practice/p36.py
@@ -0,0 +1,78 @@
+import heapq
+
+
+# initializing list
+li = [5, 7, 9, 1, 3]
+heap_ = []
+
+for i in li:
+    heapq.heappush(heap_, i)
+
+heap_
+
+
+heapq.heapify(li)
+heapq.heappush(li, 3)
+li
+
+print(type(li))
+print(li)
+
+li.heappush(2)
+
+
+class DinnerPlates:
+    def __init__(self, capacity):
+        self.c = capacity
+        self.q = [] # :heap -> store the self.q in this -> record the available stack, will use heap to quickly find the smallest available stack
+        self.stacks = [] # :list -> record values of all stack of plates, its last nonempty stack are the rightmost position
+
+    def push(self, val):
+        # To push, we need to find the leftmost available position
+        # first, let's remove any stacks on the left that are full
+        # 1) self.q: if there is still available stack to insert plate
+        # 2) self.q[0] < len(self.stacks): the leftmost available index self.q[0] is smaller than the current size of the stacks
+        # 3) len(self.stacks[self.q[0]]) == self.c: the stack has reached full capacity
+        while self.q and self.q[0] < len(self.stacks) and len(self.stacks[self.q[0]]) == self.c:
+            # we remove the filled stack from the queue of available stacks
+            heapq.heappop(self.q)
+
+        # now we reach the leftmost available stack to insert
+
+        # if the q is empty, meaning there are no more available stacks
+        if not self.q:
+            # open up a new stack to insert
+            heapq.heappush(self.q, len(self.stacks))
+
+        # for the newly added stack, add a new stack to self.stacks accordingly
+        if self.q[0] == len(self.stacks):
+            self.stacks.append([])
+
+        # append the value to the leftmost available stack
+        self.stacks[self.q[0]].append(val)
+
+    def pop(self):
+        # To pop, we need to find the rightmost nonempty stack
+        # 1) stacks is not empty (self.stacks) and
+        # 2) the last stack is empty
+        while self.stacks and not self.stacks[-1]:
+            # we throw away the last empty stack, because we can't pop from it
+            self.stacks.pop()
+
+        # now we reach the rightmost nonempty stack
+
+        # we pop the plate from the last nonempty stack of self.stacks by using popAtStack function
+        return self.popAtStack(len(self.stacks) - 1)
+
+    def popAtStack(self, index):
+        # To pop from a stack of given index, we need to make sure that it is not empty
+        # 1) the index for inserting is valid and，
+        # 2) the stack of interest is not empty
+        if 0 <= index < len(self.stacks) and self.stacks[index]:
+            # we add the index into the available stack
+            heapq.heappush(self.q, index)
+            # take the top plate, pop it and return its value
+            return self.stacks[index].pop()
+
+        # otherwise, return -1 because we can't pop any plate
+        return -1
