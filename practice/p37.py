451b62a4f8cc0f46dbfa774c2d39750773dfa6cd
Mitra Mirshafiee <mitra.mirshafiee@gmail.com>
2022-11-10 20:41:19 -0700

    second leet

--- practice/p37.py
+++ practice/p37.py
@@ -125,3 +125,135 @@ a = [0, 1, 1, 0]
 [0,0,0,0,1], 1 
 [0,0,0,1,0], 2
 [0,0,1,0,0], 3
+
+
+
+
+
+l = [1,2,3,4,5]
+
+def get_sub(l):
+    ret_list = []
+    subset = []
+
+    def dfs(i):
+        if i >= len(l):
+            ret_list.append(subset.copy())
+            return 
+
+        subset.append(l[i])
+        dfs(i+1)
+        subset.pop()
+        dfs(i+1)
+
+    dfs(0)
+
+    return ret_list 
+
+get_sub(l)
+
+
+
+def get_num_paths(n, m):
+    """
+    n: number of cols
+    m: number of rows
+    """
+    row = n * [1]
+
+    for i in range(m-2, -1, -1):
+        new_row = n * [1]
+        for j in range(n-2, -1, -1):
+            new_row[j] = new_row[j+1] + row[j]
+        row = new_row
+    return row[0]
+
+
+
+
+
+def get_nums_path_with_obs(grid):
+    n = len(grid[0])
+    m = len(grid)
+
+    grid2 = [[0]*n for _ in range(m)]
+
+    def helper(i, j):
+        if i>=m or j>=n or grid[i][j]: return 0
+        if i == m-1 or j== n-1: return 1
+        grid2[i][j] = helper(i, j+1) + helper(i+1, j)
+        return grid2[i][j]
+
+    helper(0, 0)
+    return grid2[0][0]
+
+dividend = 231
+divisor = 3
+import math 
+
+
+
+math.exp(math.log(dividend) - math.log(divisor))
+
+
+
+'''
+
+i <<=1 is the same as i = i * 2, and temp <<= 1, is the same as temp *=2
+
+The inner loop tries to subtract with (2* divisor), (4 * divisor) and so on from dividend until the dividend is smaller than the 2n * divisor. Once dividend is smaller than 2n*divisor, we set n = 1 and start again.
+
+Let's take an example: 50 / 4
+At the start,
+temp, i = divisor, 1 # dividend = 50, temp = 4, i = 1
+dividend -= temp # dividend = 46, temp = 4 , i = 1
+res += i # res = 1
+i <<= 1 # dividend = 46, temp = 4 , i = 2
+temp <<= 1 # dividend = 46, temp = 8 , i = 2
+
+Second iteration:
+dividend -= temp # dividend = 38, temp = 8 , i = 2
+res += i # res = 3
+i <<= 1 # dividend = 38, temp = 8 , i = 3
+temp <<= 1 # dividend = 38, temp = 12 , i = 3
+
+and so on, when dividend > temp, we start over again with temp = 4, and i = 1
+'''
+
+def divide(divisor, dividend):
+
+    positive = (divisor < 0) == (dividend < 0)
+    dividend, divisor = abs(dividend), abs(divisor)
+    res = 0
+
+    while dividend <= divisor:
+        temp, i = divisor, 1
+
+        while dividend <= temp:
+            dividend -= temp
+            res += i
+            i <<= 1
+            temp <<= 1
+    res = res if positive else -res
+    return min(max(res, -2147483648), 2147483647)
+
+
+def permute(nums):
+
+    res = []
+
+    if len(nums) == 1:
+        return [nums[:]] # what's the difference with nums.copy()?
+
+    for _ in range(len(nums)):
+        n = nums.pop(0)
+        perms = permute(nums)
+        for perm in perms:
+            perm.append(n)
+        
+        res.extend(perms)
+        nums.append(n)
+    return res
+
+
+permute(nums=[1,2,3,4])
